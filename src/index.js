95b8182ac12e064f1130004259d071f21cd8fc2b
vaivankov <ivankov.vitalii.2017@yandex.ru>
2020-09-25 13:11:56 +0300

    Create functions for saving state

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {Toolbar} from "./components/toolbar/Toolbar";
 import {Formula} from "./components/formula/Formula";
 import {Table} from "./components/table/Table";
 import {createStore} from "./core/createStore";
+import {storage} from "./core/utils";
 import {rootReducer} from "./redux/rootReducer";
 
 const store = createStore(
@@ -13,6 +14,17 @@ const store = createStore(
       colState: {},
     }
 );
+
+store.subscribe((state) => {
+  console.log(
+      'AppState',
+      state
+  );
+  storage(
+      'excel-state',
+      state
+  );
+});
 
 const excel = new Excel(
     "#app",
