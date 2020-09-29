fa54aab58dc29fd063f1dd7e9337a8c255fa56f3
vaivankov <ivankov.vitalii.2017@yandex.ru>
2020-09-29 17:40:23 +0300

    Create and link state for Toolbar

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,8 +1,8 @@
-import {ExcelComponent} from '@core/ExcelComponent';
 import {$} from '../../core/dom';
+import {ExcelStateComponent} from '../../core/ExcelStateComponent';
 import {createToolbar} from './toolbar_template';
 
-export class Toolbar extends ExcelComponent {
+export class Toolbar extends ExcelStateComponent {
   static className = 'excel__toolbar'
 
   constructor($root, options) {
@@ -16,14 +16,30 @@ export class Toolbar extends ExcelComponent {
     );
   }
 
+  prepare() {
+    const initialState = {
+      textAlign: 'left',
+      fontWeight: 'normal',
+      textDecoration: 'none',
+      fontStyle: 'normal',
+    };
+    this.initState(initialState);
+  }
+
+  get template() {
+    return createToolbar(this.state);
+  }
+
   toHTML() {
-    return createToolbar();
+    return this.template;
   }
 
   onClick(event) {
     const $target = $(event.target);
     if ($target.data.type === 'button') {
-      console.log($target.text());
+      const value = JSON.parse($target.data.value);
+      const key = Object.keys(value)[0];
+      this.setState({[key]: value[key]});
     }
   }
 }
